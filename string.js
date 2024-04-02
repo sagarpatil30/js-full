172ae2aaa417491df79a1fe59dbee54b1f19167e
sagar30 <sagarpatil30720@gmail.com>
2024-04-02 14:48:48 +0530

    string with template literals

--- string.js
+++ string.js
@@ -5,3 +5,7 @@ const salPay = 12;
 console.log(
   `im the working as ${fname} and my role is ${role} and my pay is ${salPay} i work in ${companyName}`
 );
+
+
+const lname = `name lastname is ${'patil'} and my house number is ${"0202"}`
+console.log(lname);
