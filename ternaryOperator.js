4146bc6bbc26a3c9de87f3d3dd96f7e9ba3016d0
sagar30 <sagarpatil30720@gmail.com>
2024-04-02 17:29:08 +0530

    ternaryOperator

---
+++ ternaryOperator.js
@@ -0,0 +1,8 @@
+const age = 20;
+age > 10 ? console.log('correct age') : console.log('below age')
+
+let name = "cat"
+name === "dog" ? console.log('my pet is cat') : console.log('my pet is not cat')
+
+const drink = 18;
+drink <= 18 ? console.log('Not allow to drink') : console.log(' allowed to drink')
