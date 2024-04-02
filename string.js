e6b0283e2e23ff463689aafe038f630e57bbbe41
sagar30 <sagarpatil30720@gmail.com>
2024-04-02 14:46:34 +0530

    string with template literals

---
+++ string.js
@@ -0,0 +1,7 @@
+const fname = "tester";
+const role = " testing application";
+const companyName = " abc";
+const salPay = 12;
+console.log(
+  `im the working as ${fname} and my role is ${role} and my pay is ${salPay} i work in ${companyName}`
+);
