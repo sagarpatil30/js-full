96577d8eccfadc1e9db301e3a5eadfc3fcd5011e
sagar30 <sagarpatil30720@gmail.com>
2024-04-02 14:58:32 +0530

    if else

---
+++ if-else.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 20;
+if (a > b) {
+console.log('greater');
+}else{
+console.log('lesser');
+}
+
+let yearOfBirth = 1999;
+let year = 2024
+
+if(year - yearOfBirth){
+console.log(`my birth year ${yearOfBirth}`);
+}else{
+console.log('wrong birth year');
+}
