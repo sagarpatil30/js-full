6115c2ea60c259facc01407005c45c3ab8950d45
sagar30 <sagarpatil30720@gmail.com>
2024-04-02 17:18:42 +0530

    swtich and booleanLogic

---
+++ BooleanLogic.js
@@ -0,0 +1,16 @@
+// & || !
+
+// in && operator both the cases must be true
+// True when All are true
+const bike = true;
+const car = false;
+// const car = true;
+console.log(car && bike);
+
+// in or operator anyone case must be true 
+// True when anyone is true
+
+const bat = true;
+const ball = false;
+console.log(bat || ball);
+console.log(!bat);
---
+++ switch.js
@@ -0,0 +1,15 @@
+const day ='monday' 
+
+switch(day){
+    case 'monday': 
+    console.log('have to hit the office');
+    console.log('give best in office');
+    break;
+
+    case 'tuesday':
+    console.log('attend the meetings');
+    break;
+
+    default:
+        console.log('No work');
+}
