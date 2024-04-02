732849b691a1d920491898f1a16982a7548dfdad
sagar30 <sagarpatil30720@gmail.com>
2024-04-02 14:42:45 +0530

    first challenge

---
+++ first_challenge.js
@@ -0,0 +1,12 @@
+// Body wieght mass of two people
+const personOneWeight = 45;
+const personOneHeight = 5.5;
+
+const personTwoHeight = 6;
+const personTwoWeight = 55;
+
+// apply the formula to calculate the weight 
+const BMIpersonOne = personOneWeight / personOneHeight ** 2;
+const BMIpersonTwo = personTwoWeight / (personTwoHeight * personTwoHeight)
+console.log(BMIpersonOne);
+console.log(BMIpersonTwo);
