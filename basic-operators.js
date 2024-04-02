2cdac78774ea2c98dcc9400ee1c75b2cc4986c59
sagar30 <sagarpatil30720@gmail.com>
2024-04-02 11:54:51 +0530

    basic-operators

--- basic-operators.js
+++ basic-operators.js
@@ -1,3 +1,49 @@
 let a = 2000;
-let b = a-500;
+let b = a - 500;
 console.log(b);
+
+// we can us all kind of math operations
+
+const per = 200;
+const month = per * 5;
+console.log(month / 2);
+
+// we can log multiple variables
+console.log(per, month);
+
+// number of times
+let c = 3;
+let d = c ** 3;
+console.log(d);
+
+
+// joins
+let fname = "java";
+let lname = "script";
+console.log(fname+" "+lname);
+console.log(fname+lname);
+
+// assignment operators
+
+// x holds the value as 15
+let x = 10 +5;
+// 15 + 10
+x += 10;
+// 15 - 5
+x -= 5;
+// 15 / 2 = 7.5
+x /= 2;
+x *= 2;
+console.log(x);
+
+
+let m = 10;
+m++;
+m--;
+console.log(m);
+
+
+// comparison operators
+const numOne = 20;
+const numTwo = 20;
+console.log(numTwo <= numOne);
